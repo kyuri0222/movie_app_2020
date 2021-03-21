af5d2451aadf0f7547f0a017dccc5c3d7a3c3bc3
kyurikim <kuri222@naver.com>
2021-03-21 23:12:30 +0900

    Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import {HashRouter, Route} from 'react-router-dom';
+import About from './routes/About';
 
 function App() {
     return (
         <HashRouter>
-        <Route />
+        <Route path="/about" component={About}/>
         </HashRouter>
     );
 }
